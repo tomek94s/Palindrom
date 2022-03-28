91f76a33a3237d51befc7d97d0b727d57e9c632e
tomek94s <tomasz.siemczyk@monterail.com>
2022-03-28 22:31:10 +0200

    palindrom

---
+++ 4.2.py
@@ -0,0 +1,10 @@
+def palindrom(x):
+    """funkcja sprawdza czy słowo x jest palindromem"""
+    return x == x[::-1]
+    
+x = "pipa" #w tym miejscu nalezy podac sprawdzane slowo
+
+if palindrom(x) == True:
+    print("slowo" + " jest palindromem")
+elif palindrom(x) == False:
+    print("slowo"  + " nie jest palindromem")
